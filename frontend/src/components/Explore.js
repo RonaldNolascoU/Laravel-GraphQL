cc829903361b3d6112daf030e5a3d9c56a11d9d3
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2020-09-03 21:58:40 -0600

    feat(search): Added user search

--- frontend/src/components/Explore.js
+++ frontend/src/components/Explore.js
@@ -2,27 +2,55 @@ import React, { useState, useRef, useEffect } from 'react';
 import "./Explore.css";
 import SearchIcon from "@material-ui/icons/Search";
 import { CSSTransition } from 'react-transition-group';
+
+import { gql, useQuery, useLazyQuery } from '@apollo/client';
+
+const GET_USERS = gql`
+    query getUsers($name: String!) {
+        users(name: $name) {
+        name
+        avatar
+        }
+    }
+`;
+
 function Explore() {
     const [open, setOpen] = useState(false);
+    const [text, setTextToSearch] = useState('');
+    const [getUsers, {loading, data, error}] = useLazyQuery(GET_USERS);
+    console.log(data)
+
+    useEffect(() => {
+        getUsers({
+            variables: {
+                name: '%%'
+            }
+        })
+    }, [])
+
     return (
         <div className="explore" >
             <div className="explore__header">
                 <div className="explore__input">
                     <SearchIcon className="explore__searchIcon" />
-                    <input placeholder="Search Twitter" type="text" onFocus={() => {setOpen(!open)}}/>
+                    <input placeholder="Search Twitter" type="text" onFocus={() => {setOpen(!open)}} onKeyUp={(e) => {
+                        getUsers({
+                            variables: {
+                                name: '%' +e.target.value + '%'
+                            }
+                        })}}/>
                 </div>
             </div>
             <div className="users__dropdown">
             {open && 
-                <DropdownMenu></DropdownMenu>
+                <DropdownMenu user={data && data.users}></DropdownMenu>
             }
             </div>
         </div>
     )
 }
 
-function DropdownMenu() {
-
+function DropdownMenu({user}) {
     const [activeMenu, setActiveMenu] = useState('main');
     const [menuHeight, setMenuHeight] = useState(null);
     const dropdownRef = useRef(null);
@@ -38,9 +66,13 @@ function DropdownMenu() {
     function DropdownItem(props) {
         return (
             <a className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
-                <span className="icon-button">{props.leftIcon}</span>
-                {props.children}
-                <span className="icon-right">{props.rightIcon}</span>
+                {props.leftIcon &&
+                <img className="icon-button" src={props.leftIcon}/>
+                }
+                {props.noIcon &&
+                <span className="icon-button">{props.noIcon}</span>
+                }
+                <span className="user__name">{props.children}</span>
             </a>
         )
     }
@@ -53,15 +85,14 @@ function DropdownMenu() {
                 onEnter={calcHeight}
                 classNames="menu-primary">
                 <div className="menu">
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
-                    <DropdownItem leftIcon="🤔">My Profile</DropdownItem>
+                    {user && user.length > 0 ?
+                    user.map((u) => (
+                        <DropdownItem key={user.name} leftIcon={u.avatar}>{u.name}</DropdownItem>
+                        ))
+                        :
+                        <DropdownItem noIcon="❌">No users 🤔</DropdownItem>
+                    }
+
                 </div>
             </CSSTransition>
         </div>
